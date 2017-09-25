ce53ee4b79da75eb2ad14b3246cba453af322925
ikindiBy <ikindi@mail.ru>
2017-09-25 21:52:23 +0300

    fsm: refactoring

--- src/fsm.js
+++ src/fsm.js
@@ -1,160 +1,130 @@
 class FSM {
      constructor(config) {
         this.state = config.initial;
-         this.states =new Array();
-         this.states[0] = config.initial;
-         this.undoned = new Array();
-         this.countUndo=0;
-         this.callChange = false;
+        this.states = [];
+        this.states[0] = config.initial;
+        this.undoned = [];
+        this.countUndo=0;
+        this.callChange = false;
+        this.mapStates = new Map();
+        this.mapRelations = new Map();
 
-            this.mapStates = new Map();
-           
-            this.mapRelations = new Map();
+        for(let sta in config.states) {
+          this.mapStates.set(sta, config.states[sta]);
+        };
 
-            for(var sta in config.states) {
-            this.mapStates.set(sta, config.states[sta]);
-            }
+        for(let statKey of this.mapStates.keys()) { 
+          let mapEvents = new Map();
+          for(let eve in (this.mapStates.get(statKey)).transitions ) {
+            mapEvents.set(eve, (this.mapStates.get(statKey)).transitions[eve]);
+          }
+          this.mapRelations.set(statKey, mapEvents);  
+        };
+    };
 
-            for(var statKey of this.mapStates.keys()) { 
-                 var mapEvents = new Map();
-                for(var eve in (this.mapStates.get(statKey)).transitions ) {
-                mapEvents.set(eve, (this.mapStates.get(statKey)).transitions[eve]);
-                }
-            this.mapRelations.set(statKey, mapEvents);  
-            }
-    }
-
-    getState() {
+  getState() {
         return this.state;
-    }
+    };
 
 
-changeState(state) {
-    var except = true;
+  changeState(state) {
+    let except = true;
     try{
-        for(var statKey of this.mapRelations.keys()) {
-        if (state == statKey){
-        this.state = state;
-        this.states.push(state);
-        this.callChange = true;
-        except = false;
-      }
-    }
+        for(let statKey of this.mapRelations.keys()) {
+            if (state == statKey){
+            this.state = state;
+            this.states.push(state);
+            this.callChange = true;
+            except = false;
+          }
+        }
         if(except){
-         
-        throw new Error("It isn't correct state!")
+        throw new Error("It isn't correct state!");
+        }
+      } catch (e){
+         alert(e.name+" "+e.message);
       }
-    }
-       catch (e){
-         alert(e.name+" "+e.message)
+    };
 
-      }
-    }
-
-    // trigger(event) {
-    //     var currentState = this.state;
-    //     for(var statKey of this.mapRelations.keys()) { 
-    //         if(statKey == currentState){
-    //              for(var eve of (this.mapRelations.get(statKey)).keys()) { 
-    //                 if(eve == event){
-    //                    this.changeState(this.mapRelations.get(statKey).get(eve));
-    //                 }
-    //             }
-    //         }      
-    //     }       
-    // }
-
-
-
- trigger(event) {
+  trigger(event) {
       try{
-         var except = true;
-        var currentState = this.state;
-        for(var statKey of this.mapRelations.keys()) { 
+        let except = true;
+        let currentState = this.state;
+        for(let statKey of this.mapRelations.keys()) { 
             if(statKey == currentState){
-                 for(var eve of (this.mapRelations.get(statKey)).keys()) { 
+                 for(let eve of (this.mapRelations.get(statKey)).keys()) { 
                     if(eve == event){
-                       this.changeState(this.mapRelations.get(statKey).get(eve));
-                       except = false;
+                      this.changeState(this.mapRelations.get(statKey).get(eve));
+                      except = false;
                     } 
                 }
-
-            } 
-               
+            }     
         }
-         if (except) {
-                      throw new Error("It isn't correct event!")
-                    }    
-        }
-       catch (e){
-         alert(e.name+" "+e.message)
-
+        if (except) {
+          throw new Error("It isn't correct event!");
+        }    
+      } catch (e){
+        alert(e.name+" "+e.message);
       }      
-    }
+    };
 
-
-     reset() {
-        var initStat = this.states[0];
-        this.states.length = 0;
-        this.changeState(initStat);
-        this.countUndo = 0;
-        this.callChange = false;
-    }
+    reset() {
+      let initStat = this.states[0];
+      this.states.length = 0;
+      this.changeState(initStat);
+      this.countUndo = 0;
+      this.callChange = false;
+    };
 
     getStates(event) {
-        var arrStatForEvent = new Array();
-
-        if (event == undefined) {
-            for(var statKey of this.mapRelations.keys()) { 
-                arrStatForEvent.push(statKey);
-            }
-        } else {
-            for(var statKey of this.mapRelations.keys()) { 
-                 for(var eve of (this.mapRelations.get(statKey)).keys()) { 
-                    if(event == eve){
-                              arrStatForEvent.push(statKey);
-                    }  
-                }      
-            } 
-        }
-        return arrStatForEvent;       
-    }
+      let arrStatForEvent = [];
+      if (event == undefined) {
+          for(let statKey of this.mapRelations.keys()) { 
+              arrStatForEvent.push(statKey);
+          }
+      } else {
+          for(let statKey of this.mapRelations.keys()) { 
+               for(let eve of (this.mapRelations.get(statKey)).keys()) { 
+                  if(event == eve){
+                    arrStatForEvent.push(statKey);
+                  }  
+              }      
+          } 
+      }
+      return arrStatForEvent;       
+    };
 
     undo() {
-        var num = this.states.length;
-        if (num > 1){
-            this.undoned.push(this.states[num-1]);
-            this.countUndo++;
-            this.state = this.states[num-2];
-            this.states.length = num-1;
-            this.callChange = false;
-            return true;
-        }else {
-            return false;
-        }
-    }
+      let num = this.states.length;
+      if (num > 1){
+        this.undoned.push(this.states[num-1]);
+        this.countUndo++;
+        this.state = this.states[num-2];
+        this.states.length = num-1;
+        this.callChange = false;
+        return true;
+      } else {
+        return false;
+      }
+    };
 
     redo() {
-        if(this.countUndo<=0 || this.callChange == true){
-            return false;
-        }else {
-         this.changeState(this.undoned[this.countUndo-1]);
-         this.countUndo--;
-            return true; 
-        }
-
-
-    }
-
+      if(this.countUndo<=0 || this.callChange == true){
+        return false;
+      } else {
+        this.changeState(this.undoned[this.countUndo-1]);
+        this.countUndo--;
+        return true; 
+      }
+    };
 
     clearHistory() {
-        var currentState = this.states[(this.states.length-1)];
-        this.states.length = 0;
-        this.changeState(currentState);
-        this.countUndo = 0;
-        this.callChange == true;
-
-    }
+      let currentState = this.states[(this.states.length-1)];
+      this.states.length = 0;
+      this.changeState(currentState);
+      this.countUndo = 0;
+      this.callChange == true;
+    };
 }
 
 module.exports = FSM;
